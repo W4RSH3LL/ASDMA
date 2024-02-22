e91fbb28c42584d84fd932fe3246a00848d565eb
SMC <samuel.mccalla38@gmail.com>
2024-02-22 11:18:48 +0100

    modifier l arborescense

---
+++ basic-setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install -y git python3-pip
+git clone https://github.com/W4RSH3LL/ASDMA.git
+cd ASDMA
+pip install -r requirements.txt
+cd ../ansible
+ansible-playbook deploy-ansible.yml -i inventory.ini
